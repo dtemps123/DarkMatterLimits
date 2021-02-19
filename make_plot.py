ce4ae0f27a33c52ebe96908068cfa903f6026093
dylan <temples@u.northwestern.edu>
2021-02-19 14:42:38 -0600

    general updates to making the plot

--- make_plot.py
+++ make_plot.py
@@ -31,13 +31,41 @@ pyp.rc('font', family='serif')
 default_colors = pyp.rcParams['axes.prop_cycle'].by_key()['color']
 ## ==================================================== ##
 
-test = RC.ResultCurve("X1T_MIGDAL_2020.dat")
+CDEX10    = RC.ResultCurve("CDEX10_2018.dat")
+CDMSLite  = RC.ResultCurve("CDMSLite_2016.dat")
+CRESSTII  = RC.ResultCurve("CRESSTII_2015.dat")
+CRESSTIII = RC.ResultCurve("CRESSTIII_2017.dat")
+CRESSTsrf = RC.ResultCurve("CRESSTsurface_2017.dat")
+DarkSide  = RC.ResultCurve("DarkSide50_S2only_2018.dat")
+LUX       = RC.ResultCurve("LUX_completeExposure_2016.dat")
+NEWSG     = RC.ResultCurve("NEWS_G_2018.dat")
+PANDAX    = RC.ResultCurve("PandaX_2017.dat")
+PICO_C3F8 = RC.ResultCurve("PICO_C3F8_2017.dat")
+PICO_CF3I = RC.ResultCurve("PICO_CF3I_2015.dat")
+X1T_MIG   = RC.ResultCurve("X1T_MIGDAL_2020.dat")
+XENON1T   = RC.ResultCurve("XENON1T_2018.dat")
+XENON100  = RC.ResultCurve("XENON100S2_2016.dat")
+XMASS     = RC.ResultCurve("XMASS_2018.dat")
 
 ## ==================================================== ##
 fig, ax0 = pyp.subplots(1,1, figsize = (9,7))
 
 ## Add the curves
-test.plot_curve(fig)
+CDEX10.plot_curve(fig)
+CDMSLite.plot_curve(fig)
+CRESSTII.plot_curve(fig)
+CRESSTIII.plot_curve(fig)
+CRESSTsrf.plot_curve(fig)
+DarkSide.plot_curve(fig)
+LUX.plot_curve(fig)
+NEWSG.plot_curve(fig)
+PANDAX.plot_curve(fig)
+PICO_C3F8.plot_curve(fig)
+PICO_CF3I.plot_curve(fig)
+X1T_MIG.plot_curve(fig)
+XENON1T.plot_curve(fig)
+# XENON100.plot_curve(fig)
+XMASS.plot_curve(fig)
 
 ## Set the plot scales
 ax0.set_xscale('log')
