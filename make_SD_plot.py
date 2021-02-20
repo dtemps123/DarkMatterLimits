5c5346929ec4a6948b872053d99969bd2abc1ebe
dylan <temples@u.northwestern.edu>
2021-02-19 19:36:54 -0600

    add curves

--- make_SD_plot.py
+++ make_SD_plot.py
@@ -29,6 +29,8 @@ default_colors = pyp.rcParams['axes.prop_cycle'].by_key()['color']
 ## ==================================================== ##
 
 ## Result curves
+CDMSlte_n = RC.ResultCurve("SD_CDMSLite_n_2016.dat")
+CDMSlte_p = RC.ResultCurve("SD_CDMSLite_p_2016.dat")
 LUX_n     = RC.ResultCurve("SD_LUX_n_2017.dat")
 LUX_p     = RC.ResultCurve("SD_LUX_p_2017.dat")
 PANDAX_n  = RC.ResultCurve("SD_PandaX_n_2016.dat")
@@ -46,27 +48,28 @@ X1T_p_mig = RC.ResultCurve("SD_X1T_p_migdal.dat")
 NuFog     = NFC.NeutrinoFog("SI_NeutrinoFloor_Ruppin_LZ_Fig3_1000ty.dat")
 
 ## Calculate the excluded parameter space
-plot_x_limits = n.array([ 1e0   , 1e3   ])
-plot_y_limits = n.array([ 1e-42 , 1e-36 ])
+plot_x_limits = n.array([ 1e-1   , 1e3   ])
+plot_y_limits = n.array([ 1e-42 , 1e-30 ])
 
 x_val_arr     = n.logspace( start = n.log10(plot_x_limits[0]),
 							stop  = n.log10(plot_x_limits[1]),
 							num   = 1000)
-interp_array_p  = n.zeros(shape=(2,1000))
-interp_array_n  = n.zeros(shape=(2,1000))
+interp_array_p  = n.zeros(shape=(4,1000))
+interp_array_n  = n.zeros(shape=(4,1000))
 
-interp_array_p[0,:] = PICO_C3F8.interpolator(n.power(x_val_arr,1))
-interp_array_p[1,:] = PICASSO.interpolator(n.power(x_val_arr,1))
+interp_array_p[0,:] = CDMSlte_p.interpolator(n.power(x_val_arr,1))
+interp_array_p[1,:] = PICO_C3F8.interpolator(n.power(x_val_arr,1))
+interp_array_p[2,:] = PICASSO.interpolator(n.power(x_val_arr,1))
+interp_array_p[3,:] = X1T_p_mig.interpolator(n.power(x_val_arr,1))
 
 exp_upper_lim_p     = n.min(interp_array_p, axis=0)
 
-interp_array_n[0,:] = XENON1T_n.interpolator(n.power(x_val_arr,1))
-interp_array_n[1,:] = X1T_n_low.interpolator(n.power(x_val_arr,1))
+interp_array_n[0,:] = CDMSlte_n.interpolator(n.power(x_val_arr,1))
+interp_array_n[1,:] = XENON1T_n.interpolator(n.power(x_val_arr,1))
+interp_array_n[2,:] = X1T_n_low.interpolator(n.power(x_val_arr,1))
+interp_array_n[3,:] = X1T_n_mig.interpolator(n.power(x_val_arr,1))
 
 exp_upper_lim_n     = n.min(interp_array_n, axis=0)
-
-
-
 
 ## ==================================================== ##
 fig = pyp.figure(figsize = (9,4))
@@ -79,6 +82,7 @@ gs.update(wspace=0.325, hspace=0.075)
 ax0 = fig.add_subplot(gs[0])
 
 ## Add the result curves
+CDMSlte_p.plot_curve(fig)
 LUX_p.plot_curve(fig)
 PANDAX_p.plot_curve(fig)
 PICO_C3F8.plot_curve(fig)
@@ -116,6 +120,7 @@ ax0.yaxis.set_tick_params(top = 'on', which='major')
 ax1 = fig.add_subplot(gs[1])
 
 ## Add the result curves
+CDMSlte_n.plot_curve(fig)
 LUX_n.plot_curve(fig)
 PANDAX_n.plot_curve(fig)
 X1T_n_mig.plot_curve(fig)
