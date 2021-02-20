a970c3d3fce8c4045781b212e07a088c497d7753
dylan <temples@u.northwestern.edu>
2021-02-20 03:51:37 -0600

    adjust axis lable, add electron scattering file

--- make_eDM_plot.py
+++ make_eDM_plot.py
@@ -29,47 +29,38 @@ default_colors = pyp.rcParams['axes.prop_cycle'].by_key()['color']
 ## ==================================================== ##
 
 ## Result curves
-CDMSlte_n = RC.ResultCurve("SD_CDMSLite_n_2016.dat")
-CDMSlte_p = RC.ResultCurve("SD_CDMSLite_p_2016.dat")
-LUX_n     = RC.ResultCurve("SD_LUX_n_2017.dat")
-LUX_p     = RC.ResultCurve("SD_LUX_p_2017.dat")
-PANDAX_n  = RC.ResultCurve("SD_PandaX_n_2016.dat")
-PANDAX_p  = RC.ResultCurve("SD_PandaX_p_2016.dat")
-PICASSO   = RC.ResultCurve("SD_PICASSO_2016.dat")
-PICO_C3F8 = RC.ResultCurve("SD_PICO60_C3F8.dat")
-PICO_CF3I = RC.ResultCurve("SD_PICO_CF3I.dat")
-XENON1T_n = RC.ResultCurve("SD_XENON1T_n_2019.dat")
-XENON1T_p = RC.ResultCurve("SD_XENON1T_p_2019.dat")
-X1T_n_low = RC.ResultCurve("SD_XENON1T_n_lowmass.dat")
-X1T_n_mig = RC.ResultCurve("SD_X1T_n_migdal.dat")
-X1T_p_mig = RC.ResultCurve("SD_X1T_p_migdal.dat")
+DAMIC_FDM1   = RC.ResultCurve("eDM_DAMIC_FDM1_2019.dat")
+PANDAX_FDM1  = RC.ResultCurve("eDM_PandaX_FDM1_2021.dat")
+CDMS_FDM1    = RC.ResultCurve("eDM_SuperCDMS_FDM1_2018.dat")
+X1T_FDM1     = RC.ResultCurve("eDM_XENON1T_FDM1_2019.dat")
 
-## Neutrino Fog curves
-NuFog     = NFC.NeutrinoFog("SI_NeutrinoFloor_Ruppin_LZ_Fig3_1000ty.dat")
+DAMIC_FDMq2  = RC.ResultCurve("eDM_DAMIC_FDMq2_2019.dat")
+PANDAX_FDMq2 = RC.ResultCurve("eDM_PandaX_FDMq2_2021.dat")
+CDMS_FDMq2   = RC.ResultCurve("eDM_SuperCDMS_FDMq2_2018.dat")
 
 ## Calculate the excluded parameter space
-plot_x_limits = n.array([ 1e-1   , 1e3   ])
-plot_y_limits = n.array([ 1e-42 , 1e-30 ])
+plot_x_limits       = n.array([ 2e-1   , 2e3   ])
+plot_y_limits_FDM1  = n.array([ 1e-42  , 1e-25 ])
+plot_y_limits_FDMq2 = n.array([ 1e-35  , 1e-27 ])
 
 x_val_arr     = n.logspace( start = n.log10(plot_x_limits[0]),
 							stop  = n.log10(plot_x_limits[1]),
 							num   = 1000)
-interp_array_p  = n.zeros(shape=(4,1000))
-interp_array_n  = n.zeros(shape=(4,1000))
+interp_array_FDM1   = n.zeros(shape=(4,1000))
+interp_array_FDMq2  = n.zeros(shape=(3,1000))
 
-interp_array_p[0,:] = CDMSlte_p.interpolator(n.power(x_val_arr,1))
-interp_array_p[1,:] = PICO_C3F8.interpolator(n.power(x_val_arr,1))
-interp_array_p[2,:] = PICASSO.interpolator(n.power(x_val_arr,1))
-interp_array_p[3,:] = X1T_p_mig.interpolator(n.power(x_val_arr,1))
+interp_array_FDM1[0,:]  = DAMIC_FDM1.interpolator(n.power(x_val_arr,1))
+interp_array_FDM1[1,:]  = PANDAX_FDM1.interpolator(n.power(x_val_arr,1))
+interp_array_FDM1[2,:]  = CDMS_FDM1.interpolator(n.power(x_val_arr,1))
+interp_array_FDM1[3,:]  = X1T_FDM1.interpolator(n.power(x_val_arr,1))
 
-exp_upper_lim_p     = n.min(interp_array_p, axis=0)
+exp_upper_lim_FDM1      = n.min(interp_array_FDM1, axis=0)
 
-interp_array_n[0,:] = CDMSlte_n.interpolator(n.power(x_val_arr,1))
-interp_array_n[1,:] = XENON1T_n.interpolator(n.power(x_val_arr,1))
-interp_array_n[2,:] = X1T_n_low.interpolator(n.power(x_val_arr,1))
-interp_array_n[3,:] = X1T_n_mig.interpolator(n.power(x_val_arr,1))
+interp_array_FDMq2[0,:] = DAMIC_FDMq2.interpolator(n.power(x_val_arr,1))
+interp_array_FDMq2[1,:] = PANDAX_FDMq2.interpolator(n.power(x_val_arr,1))
+interp_array_FDMq2[2,:] = CDMS_FDMq2.interpolator(n.power(x_val_arr,1))
 
-exp_upper_lim_n     = n.min(interp_array_n, axis=0)
+exp_upper_lim_FDMq2     = n.min(interp_array_FDMq2, axis=0)
 
 ## ==================================================== ##
 fig = pyp.figure(figsize = (9,4))
@@ -82,17 +73,13 @@ gs.update(wspace=0.325, hspace=0.075)
 ax0 = fig.add_subplot(gs[0])
 
 ## Add the result curves
-CDMSlte_p.plot_curve(fig)
-LUX_p.plot_curve(fig)
-PANDAX_p.plot_curve(fig)
-PICO_C3F8.plot_curve(fig)
-PICO_CF3I.plot_curve(fig)
-PICASSO.plot_curve(fig)
-X1T_p_mig.plot_curve(fig)
-XENON1T_p.plot_curve(fig)
+DAMIC_FDM1.plot_curve(fig)
+PANDAX_FDM1.plot_curve(fig)
+CDMS_FDM1.plot_curve(fig)
+X1T_FDM1.plot_curve(fig)
 
 ## Fill in the exclusion curve
-ax0.fill_between(x_val_arr, exp_upper_lim_p, 1e-28, 
+ax0.fill_between(x_val_arr, exp_upper_lim_FDM1, 1e-18, 
 	color  = '#aaffc3', 
 	zorder = 0, 
 	alpha  = 0.5, 
@@ -104,11 +91,11 @@ ax0.set_yscale('log')
 
 ## Set the plot limits
 ax0.set_xlim(plot_x_limits)
-ax0.set_ylim(plot_y_limits)
+ax0.set_ylim(plot_y_limits_FDM1)
 
 ## Set the axis labels
-ax0.set_xlabel('DM mass [GeV/c$^{2}$]')
-ax0.set_ylabel(r'SD DM-nucleon cross section $\sigma_\chi^\mathrm{SD}$ [cm$^{2}$]')
+ax0.set_xlabel('DM mass [MeV/c$^{2}$]')
+ax0.set_ylabel(r'DM-electron cross section $\bar{\sigma}_e$ [cm$^{2}$]')
 
 ## Turn on some extra tick marks
 ax0.xaxis.set_tick_params(top = 'on', which='minor')
@@ -120,15 +107,12 @@ ax0.yaxis.set_tick_params(top = 'on', which='major')
 ax1 = fig.add_subplot(gs[1])
 
 ## Add the result curves
-CDMSlte_n.plot_curve(fig)
-LUX_n.plot_curve(fig)
-PANDAX_n.plot_curve(fig)
-X1T_n_mig.plot_curve(fig)
-X1T_n_low.plot_curve(fig)
-XENON1T_n.plot_curve(fig)
+DAMIC_FDMq2.plot_curve(fig)
+PANDAX_FDMq2.plot_curve(fig)
+CDMS_FDMq2.plot_curve(fig)
 
 ## Fill in the exclusion curve
-ax1.fill_between(x_val_arr, exp_upper_lim_n, 1e-28, 
+ax1.fill_between(x_val_arr, exp_upper_lim_FDMq2, 1e-18, 
 	color  = '#aaffc3', 
 	zorder = 0, 
 	alpha  = 0.5, 
@@ -140,11 +124,11 @@ ax1.set_yscale('log')
 
 ## Set the plot limits
 ax1.set_xlim(plot_x_limits)
-ax1.set_ylim(plot_y_limits)
+ax1.set_ylim(plot_y_limits_FDMq2)
 
 ## Set the axis labels
-ax1.set_xlabel('DM mass [GeV/c$^{2}$]')
-ax1.set_ylabel(r'SD DM-nucleon cross section $\sigma_\chi^\mathrm{SD}$ [cm$^{2}$]')
+ax1.set_xlabel('DM mass [MeV/c$^{2}$]')
+ax1.set_ylabel(r'DM-electron cross section $\bar{\sigma}_e$ [cm$^{2}$]')
 
 ## Turn on some extra tick marks
 ax1.xaxis.set_tick_params(top = 'on', which='minor')
